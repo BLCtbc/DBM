d745cb423a9ceb506439f4084b9c433643ded79f
blc <kjt@kenneths-mbp.attlocal.net>
2018-02-26 20:54:00 -0600

    all calls to 'WhirlWarn2' changed to 'WhirlWarn';no reason for this redundancy unless expecting uncertainty--hopefully no longer the case. Timers for Whirlwind should now show up 15s and 42s after start of every normal phase. Inner demons timer set to 15s after Demon Form start

--- DBM_Serpentshrine/Leotheras.lua
+++ DBM_Serpentshrine/Leotheras.lua
@@ -32,26 +32,25 @@ Leotheras:AddBarOption("Inner Demons")
 function Leotheras:OnCombatStart(delay)
 	self.Phase = "normal";
 	demonTargets = {};
-	
+
 	self:StartStatusBarTimer(600 - delay, "Enrage", "Interface\\Icons\\Spell_Shadow_UnholyFrenzy");
 	self:ScheduleSelf(300 - delay, "EnrageWarn", 300);
 	self:ScheduleSelf(480 - delay, "EnrageWarn", 120);
 	self:ScheduleSelf(540 - delay, "EnrageWarn", 60);
 	self:ScheduleSelf(570 - delay, "EnrageWarn", 30);
 	self:ScheduleSelf(590 - delay, "EnrageWarn", 10);
-		
+
 	self:ScheduleSelf(55, "PhaseWarn");
 	self:StartStatusBarTimer(60, "Demon Form", "Interface\\Icons\\Spell_Shadow_Metamorphosis");
-	
-	self:StartStatusBarTimer(18, "Next Whirlwind", "Interface\\Icons\\Ability_Whirlwind");
-	self:ScheduleSelf(14, "WhirlWarn2");	
+	self:StartStatusBarTimer(15, "Next Whirlwind", "Interface\\Icons\\Ability_Whirlwind");
+	self:ScheduleSelf(10, "WhirlWarn");
 end
 
 function Leotheras:OnEvent(event, arg1)
 	if event == "CHAT_MSG_MONSTER_YELL" then
 		if string.find(arg1, DBM_LEO_YELL_DEMON) then
 			self.Phase = "demon";
-			
+
 			self:Announce(DBM_LEO_WARN_DEMON_PHASE, 3);
 			self:ScheduleSelf(55, "PhaseWarn");
 			self:ScheduleSelf(60, "NormalForm");
@@ -61,8 +60,8 @@ function Leotheras:OnEvent(event, arg1)
 			self:EndStatusBarTimer("Whirlwind");
 			self:EndStatusBarTimer("Next Whirlwind");
 			self:StartStatusBarTimer(60, "Normal Form", "Interface\\Icons\\INV_Weapon_ShortBlade_07");
---			self:StartStatusBarTimer(15, "Inner Demons in", "Interface\\Icons\\Spell_Shadow_ManaFeed"); -- seems to be on a random timer (but changed to a fixed timer in 2.1.0?)
---			self:ScheduleSelf(10, "DemonsSoon");
+			self:StartStatusBarTimer(15, "Inner Demons in", "Interface\\Icons\\Spell_Shadow_ManaFeed");
+			self:ScheduleSelf(10, "DemonsSoon");
 
 		elseif arg1 == DBM_LEO_YELL_SHADOW then
 			self.Phase = "normal";
@@ -71,10 +70,12 @@ function Leotheras:OnEvent(event, arg1)
 			self:UnScheduleSelf("NormalForm");
 			self:EndStatusBarTimer("Normal Form");
 			self:EndStatusBarTimer("Demon Form");
-			
-			self:StartStatusBarTimer(22.5, "Next Whirlwind", "Interface\\Icons\\Ability_Whirlwind");
-			self:ScheduleSelf(18, "WhirlWarn2");
-			
+
+			-- commenting out for now; doesn't seem necessary given whirlwind timers continue
+			-- after leo splits
+			-- self:StartStatusBarTimer(22.5, "Next Whirlwind", "Interface\\Icons\\Ability_Whirlwind");
+			-- self:ScheduleSelf(18, "WhirlWarn2");
+
 		elseif string.find(arg1, DBM_LEO_YELL_WHISPER) then
 			if (GetTime() - self.DemonSpam) > 5 then
 				if self.Options.DemonWarn then
@@ -85,39 +86,41 @@ function Leotheras:OnEvent(event, arg1)
 				end
 			end
 		end
-		
+
 	elseif event == "NormalForm" then
 		self.Phase = "normal";
-		
+
 		self:Announce(DBM_LEO_WARN_NORMAL_PHASE, 3);
-		self:ScheduleSelf(40, "PhaseWarn");
+
+		self:ScheduleSelf(55, "PhaseWarn");
 		self:EndStatusBarTimer("Normal Form");
 		self:EndStatusBarTimer("Demon Form");
-		self:StartStatusBarTimer(45, "Demon Form", "Interface\\Icons\\Spell_Shadow_Metamorphosis");
-		
-		self:StartStatusBarTimer(19, "Next Whirlwind", "Interface\\Icons\\Ability_Whirlwind");
-		
-		self:ScheduleSelf(14, "WhirlWarn2");
-		
+
+		self:StartStatusBarTimer(60, "Demon Form", "Interface\\Icons\\Spell_Shadow_Metamorphosis");
+
+		self:StartStatusBarTimer(15, "Next Whirlwind", "Interface\\Icons\\Ability_Whirlwind");
+
+		self:ScheduleSelf(10, "WhirlWarn");
+
 	elseif event == "PhaseWarn" then
 		if self.Phase == "normal" then
 			self:Announce(DBM_LEO_WARN_DEMON_PHASE_SOON, 2);
 		elseif self.Phase == "demon" then
 			self:Announce(DBM_LEO_WARN_NORMAL_PHASE_SOON, 2);
 		end
-		
+
 	elseif event == "SPELL_AURA_APPLIED" then
 		if arg1.spellId == 37640 then -- ?
 			self:SendSync("GainWW");
 		elseif arg1.spellId == 37676 then
 			self:SendSync("Demon"..tostring(arg1.destName))
 		end
-		
+
 	elseif event == "SPELL_AURA_REMOVED" then
 		if arg1.spellId == 37640 then -- ?
 			self:SendSync("FadeWW");
 		end
-	
+
 	elseif event == "SPELL_CAST_START" then
 		if arg1.spellId == 37676 then
 			self.DemonSpam = GetTime();
@@ -126,7 +129,7 @@ function Leotheras:OnEvent(event, arg1)
 			end
 			self:StartStatusBarTimer(32.5, "Inner Demons", "Interface\\Icons\\Spell_Shadow_ManaFeed");
 		end
-		
+
 	elseif event == "EnrageWarn" and type(arg1) == "number" then
 		if arg1 >= 60 then
 			self:Announce(string.format(DBM_LEO_WARN_ENRAGE, (arg1/60), DBM_MIN), 1);
@@ -162,14 +165,14 @@ function Leotheras:OnSync(msg)
 		self:EndStatusBarTimer("Next Whirlwind");
 		self:StartStatusBarTimer(12, "Whirlwind", "Interface\\Icons\\Ability_Whirlwind");
 		self:ScheduleMethod(12, "SendSync", "FadeWW");
-		
+
 	elseif msg == "FadeWW" and self.Phase ~= "demon" then
 		if self.Options.WhirlWarn then
 			self:Announce(DBM_LEO_WARN_WHIRL_FADED, 2);
 		end
-		if not self:GetStatusBarTimerTimeLeft("Demon Form") or self:GetStatusBarTimerTimeLeft("Demon Form") > 18 then
-			self:StartStatusBarTimer(20, "Next Whirlwind", "Interface\\Icons\\Ability_Whirlwind");
-			self:ScheduleSelf(15, "WhirlWarn");
+		if not self:GetStatusBarTimerTimeLeft("Demon Form") or self:GetStatusBarTimerTimeLeft("Demon Form") > 15 then
+			self:StartStatusBarTimer(15, "Next Whirlwind", "Interface\\Icons\\Ability_Whirlwind");
+			self:ScheduleSelf(10, "WhirlWarn");
 		end
 	elseif msg:sub(0, 5) == "Demon" then
 		msg = msg:sub(6);
